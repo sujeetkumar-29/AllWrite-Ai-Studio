c50949900455107a6c3ae315c13edbcdad3ac2fa
sujeet kumar <sujeetkr503@gmail.com>
2025-07-12 22:03:42 +0530

    added cloudinary and generate image feature

--- server/server.js
+++ server/server.js
@@ -3,8 +3,11 @@ import cors from 'cors'
 import 'dotenv/config'
 import { clerkMiddleware, requireAuth } from '@clerk/express'
 import aiRouter from './routes/aiRoutes.js'
+import connectCloudinary from './configs/cloudinary.js'
 
 const app = express()
+
+await connectCloudinary()
 app.use(cors())
 app.use(express.json())
 app.use(clerkMiddleware())
