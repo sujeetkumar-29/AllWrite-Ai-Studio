29af46c9574faae45e891b1b63c18e68503065a3
sujeet kumar <sujeetkr503@gmail.com>
2025-07-13 22:44:34 +0530

    connected backend to frontend with axios and generated articles

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,15 +12,16 @@ import ReviewResume from './pages/ReviewResume'
 import Community from './pages/Community'
 import { useAuth } from '@clerk/clerk-react'
 import { useEffect } from 'react'
+import { Toaster } from 'react-hot-toast'
 
 const App = () => {
-  const { getToken } = useAuth()
-
-  useEffect(() => {
-    getToken().then((token)=>console.log(token))
-  }, [])
-  return (
+  // const { getToken } = useAuth()
+  // useEffect(() => {
+  //   getToken().then((token)=>console.log(token))
+  // }, [])
+  return ( 
     <div>
+      <Toaster />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/ai" element={<Layout />} >
